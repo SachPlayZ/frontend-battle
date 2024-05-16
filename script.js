55fffeef3850f0a103b2545bff5bd786684533e3
Sachindra Kumar Singh <45076467+SachPlayZ@users.noreply.github.com>
2024-05-16 20:32:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const cursorDot = document.querySelector("[data-cursor-dot]");
-    const cursorOutline = document.querySelector(" [data-cursor-outline]");
-    window.addEventListener("mousemove", function (e) {
-    const posX = e.clientX;
-    const posY = e.clientY;
-    cursorDot.style.left = `${posX}px`;
-    cursorDot.style.top = `${posY}px`;
-    cursorOutline.animate({
-left: `${posX}px`,
-top: `${posY}px`
-}, { duration: 500, fill: "forwards" });
+const cursorDot = document.querySelector(".cursor-dot");
+const cursorOutline = document.querySelector(".cursor-outline");
+window.addEventListener("mousemove", function (e) {
+const posX = e.clientX;
+const posY = e.clientY;
+cursorDot.style.left = `${posX}px`;
+cursorDot.style.top = `${posY}px`;
+cursorOutline.animate({
+    left: `${posX}px`,
+    top: `${posY}px`
+    }, { duration: 500, fill: "forwards" });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
     
     const scrollContainer = document.querySelector('.scroll-container');
     const scrollTextContainer = document.getElementById('scroll-text');
